63da8bbae95b3bf9778c8634d5d437bc89c1e7f9
Vipul Nikam <vipul@Vipuls-Mac.local>
2024-11-30 17:10:25 +0530

    First Iteration

--- js/map-active.js
+++ js/map-active.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 	var map;
 	map = new GMaps({
 		el: '#myMap',
-		lat: 51.507351,
-		lng: -0.127758,
+		lat: 20.5576627,
+		lng: 74.1946813,
 		scrollwheel:false,
 		zoom: 15,
 		zoomControl : false,
